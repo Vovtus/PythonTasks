2e4eb87e825d35f243d8b42a78bdc24679c88d72
vovtus <vovtuz@gmail.com>
2021-08-13 00:15:05 +0300

    test1

---
+++ Hello world1.py
@@ -0,0 +1,10 @@
+age = int (input ( "Сколько тебе лет?"))
+if (age >=25):
+    print ("Заходи с подругой!")
+elif (age >=20) and (age <=24):
+    print ("Заходи быстро!")
+elif (age >=18) and (age <=19):
+    print ("Покури за дверью!")
+else:
+    print ("Пиздуй лесом!")
+
